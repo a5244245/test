33102f4bbe0fc78c27c66998389f528da22ab0c5
a5244245 <34232331+a5244245@users.noreply.github.com>
2017-12-04 17:29:22 +0800

    Update testscript.js

--- testscript.js
+++ testscript.js
@@ -1,10 +1,10 @@
 jQuery(document).ready(function($){
 $(window).resize(function(){     
 
-       if ($('body').width() >= 768 ){
+       if ($('body').width() <= 768 ){
 
               // is mobile device
-
+              console.log('tablets');
        }
 
 });
